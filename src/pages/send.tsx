66778b4794f53fa85f93e5d1678a5d792f0ced6a
caiokronuz <caiogabriel135@gmail.com>
2023-03-21 11:31:22 -0300

    Alterações nas media queries do css da pagina home, e adição das paginas de pedido de cadastro e marketplace

---
+++ src/pages/send.tsx
@@ -0,0 +1,32 @@
+import { Header } from "@/components/Header/Header";
+import { Footer } from '@/components/Footer/Footer';
+
+import styles from '../styles/Send.module.scss';
+
+export default function sendPage() {
+    return (
+        <main className={styles.sendPage}>
+
+            <div className={styles.container}>
+
+                <Header />
+
+                <div className={styles.text}>
+                    <h1>REDUZA <strong>ATÉ 15%</strong> NA SUA <strong>CONTA DE ENERGIA</strong>!</h1>
+                    <p>Iremos analisar seu consumo e te retornaremos com um proposta. Sem compromisso nenhum!</p>
+                </div>
+                <form className={styles.forms}>
+                    <label htmlFor="name">Nome*</label>
+                    <input type="text" id="name"/>
+                    <label htmlFor="contact">Contato*</label>
+                    <input type="text" id="contact"/>
+                    <label htmlFor="bill">Conta de energia*</label>
+                    <input type="file" id="bill"/>
+                    <button type="submit">Quero economizar</button>
+                </form>
+            </div>
+
+            <Footer />
+        </main>
+    )
+}
