9054c6fc26a4dbb7be5c7c56744e2f98556c76f4
caiokronuz <caiogabriel135@gmail.com>
2023-05-12 15:38:19 -0300

    adicionado outra forma de fazer orçamento (lógica adicionada falta CSS)

--- src/pages/send.tsx
+++ src/pages/send.tsx
@@ -1,9 +1,13 @@
+import {useState} from 'react';
 import { Header } from "@/components/Header/Header";
 import { Footer } from '@/components/Footer/Footer';
 
 import styles from '../styles/Send.module.scss';
 
 export default function sendPage() {
+
+    const [billValue, setBillValue] = useState(200);
+
     return (
         <main className={styles.sendPage}>
 
@@ -15,15 +19,27 @@ export default function sendPage() {
                     <h1>REDUZA <strong>ATÉ 15%</strong> NA SUA <strong>CONTA DE ENERGIA</strong>!</h1>
                     <p>Iremos analisar seu consumo e te retornaremos com um proposta. Sem compromisso nenhum!</p>
                 </div>
+
                 <form className={styles.forms}>
+
+                    <div className={styles.sliderCounter}>
+                        <p>Qual valor médio da sua conta de luz?</p>
+                        <span>R$<p>{billValue},00</p></span>
+                        <input type="range" min="200" max="2000"  value={billValue} onChange={e => setBillValue(Number(e.target.value)) }/>
+                    </div>
+                    
                     <label htmlFor="name">Nome <span>*</span></label>
                     <input type="text" id="name" placeholder="Fulanésio Beltranosil da Silva"/>
                     <label htmlFor="contact">Telefone <span>*</span></label>
                     <input type="text" id="contact" placeholder="(85) 99999-9999"/>
-                    <label htmlFor="bill">Conta de energia <span>*</span></label>
-                    <input type="file" id="bill"/>
-                    <button type="submit">Quero economizar</button>
+
+                    <div className={styles.result}>
+                        <p>Em um ano sua economia será de:</p>
+                        <span>R$<p>{Math.floor((billValue - (billValue * 0.15)) * 12)},00</p></span>
+                        <button type="submit">Quero economizar</button>
+                    </div>
                 </form>
+
             </div>
 
             <Footer />
